fc41b451d1b53deeea25a76e8f45b730211aafce
James <james.john.xml@gmail.com>
2022-12-22 08:26:34 +0100

    Completed day 4

---
+++ Day 4.py
@@ -0,0 +1,16 @@
+class Day4:
+    def checkLeapOrNot(self, x):
+        if y % 4 == 0 and y % 100 != 0:
+            return 1
+        elif y % 400 == 0:
+            return 1
+
+print("Enter the Year: ", end="")
+y = int(input())
+
+ob = CodesCracker()
+chk = ob.checkLeapOrNot(y)
+if chk==1:
+    print("\nLeap Year")
+else:
+    print("\nNot a Leap Year")
